3a698587b28eb85d4ca76eb63748c9aa35e9b38c
Rs Rasel <rasel.mm217@gmail.com>
2023-05-07 09:55:03 +0600

    Done

---
+++ app.js
@@ -0,0 +1,58 @@
+//INPUTS
+const added = document.getElementById("added");
+const input = document.getElementById("input");
+
+// RESULT
+const result = document.getElementById("result");
+
+// BUTTONS
+const add = document.getElementById("add");
+const substract = document.getElementById("substract");
+const multiplication = document.getElementById("multiplication");
+const divition = document.getElementById("divition");
+
+// EQUAL BUTTON
+const equal = document.getElementById("equal");
+
+let addedValue = 0; // 10
+let formula = ""; // +
+
+function functionality(inp, f) {
+  addedValue = inp;
+  formula = f;
+
+  input.value=null;
+  added.innerText=inp + f;
+}
+
+function calculation(inp2) {
+  let rs = 0;
+  if (formula === "+") {
+    rs = addedValue + inp2;
+  } else if (formula === "-") {
+    rs = addedValue - inp2;
+  } else if (formula === "*") {
+    rs = addedValue * inp2;
+  } else {
+    rs = addedValue / inp2;
+  }
+  console.log(rs);
+  result.innerText = rs;
+}
+
+add.addEventListener('click',()=>{
+    functionality(parseFloat(input.value),"+");
+})
+substract.addEventListener('click',()=>{
+    functionality(parseFloat(input.value),"-");
+})
+multiplication.addEventListener('click',()=>{
+    functionality(parseFloat(input.value),"*");
+})
+divition.addEventListener('click',()=>{
+    functionality(parseFloat(input.value),"/");
+})
+
+equal.addEventListener('click',()=>{
+    calculation(parseFloat(input.value))
+})
